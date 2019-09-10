bc0381322994aa3ccf7020036e57c17cd9444988
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-09-10 18:23:14 +0100

    metrics: some docs cleanup

--- metrics/controller.go
+++ metrics/controller.go
@@ -28,13 +28,14 @@ func New(config *Config) Controller {
 }
 
 func (controller *controller) RegisterRoutes(router *gin.Engine) {
-	router.POST("/api/metrics/mixmetric", controller.CreateMixMetric)
+	router.POST("/api/metrics/mixes", controller.CreateMixMetric)
+	router.GET("/api/metrics/mixes", controller.CreateMixMetric)
 }
 
-// CreateMetric adds a node to the PKI
+// CreateMixMetric adds a node to the PKI
 // @Summary Create a metric detailing how many messages a given mixnode sent
 // @Description You'd never want to run this in production, but for demo and debug purposes it gives us the ability to generate useful visualisations of network traffic.
-// @ID createMetric
+// @ID createMixMetric
 // @Accept  json
 // @Produce  json
 // @Tags metrics
@@ -43,7 +44,24 @@ func (controller *controller) RegisterRoutes(router *gin.Engine) {
 // @Failure 400 {object} rest.Error
 // @Failure 404 {object} rest.Error
 // @Failure 500 {object} rest.Error
-// @Router /api/metrics [post]
+// @Router /api/metrics/mixes [post]
 func (controller *controller) CreateMixMetric(c *gin.Context) {
-	fmt.Println("foomp")
+	fmt.Println("CreateMixMetric")
+}
+
+// ListMixMetrics lists mixnode activity
+// @Summary Lists mixnode activity in the past 1 second
+// @Description You'd never want to run this in production, but for demo and debug purposes it gives us the ability to generate useful visualisations of network traffic.
+// @ID listMixMetrics
+// @Accept  json
+// @Produce  json
+// @Tags metrics
+// @Param   object      body   rest.ObjectRequest     true  "object"
+// @Success 200 {object} rest.ObjectIDResponse
+// @Failure 400 {object} rest.Error
+// @Failure 404 {object} rest.Error
+// @Failure 500 {object} rest.Error
+// @Router /api/metrics/mixes [get]
+func (controller *controller) ListMixMetrics(c *gin.Context) {
+	fmt.Println("ListMixMetrics")
 }
