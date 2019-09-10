c6dda6dfe0203f25b5df4444cac918f337482cd1
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-09-10 17:54:07 +0100

    metrics: documenting the createMetric route.

--- metrics/controller.go
+++ metrics/controller.go
@@ -31,6 +31,19 @@ func (controller *controller) RegisterRoutes(router *gin.Engine) {
 	router.POST("/api/metrics", controller.CreateMetric)
 }
 
+// CreateMetric adds a node to the PKI
+// @Summary Create a metric detailing how many messages a given mixnode sent
+// @Description You'd never want to run this in production, but for demo and debug purposes it gives us the ability to generate useful visualisations of network traffic.
+// @ID createMetric
+// @Accept  json
+// @Produce  json
+// @Tags metrics
+// @Param   object      body   rest.ObjectRequest     true  "object"
+// @Success 200 {object} rest.ObjectIDResponse
+// @Failure 400 {object} rest.Error
+// @Failure 404 {object} rest.Error
+// @Failure 500 {object} rest.Error
+// @Router /api/metrics [post]
 func (controller *controller) CreateMetric(c *gin.Context) {
 	fmt.Println("foomp")
 }
