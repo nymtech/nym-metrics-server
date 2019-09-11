fc1dc37c49862e03f181fd3c62e6251b491ccee8
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-09-11 14:30:36 +0100

    models: typo fix

--- models/models.go
+++ models/models.go
@@ -22,7 +22,7 @@ type UpMsg struct {
 	PubKey string `json:"pubKey" binding:"required"`
 }
 
-// Presence lets the server tell clients wahen a node was last seen
+// Presence lets the server tell clients when a node was last seen
 type Presence struct {
 	Host     string    `json:"host" binding:"required"`
 	LastSeen time.Time `json:"lastSeen" binding:"required" time_format:"unix"`
