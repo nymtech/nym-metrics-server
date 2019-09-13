00d53c347fd716e79d6a867147a5de3dc6e35d9d
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-09-13 14:51:32 +0100

    presence: upping staleness value

--- presence/db.go
+++ presence/db.go
@@ -38,18 +38,18 @@ func (db *db) get(key string) models.MixNodePresence {
 }
 
 // kill any stale presence info (older than 5 seconds)
-// TODO: kill magic number 5 by pulling this out into a config
+// TODO: kill magic number 10 by pulling this out into a config
 func (db *db) killOldsters() {
 	for key := range db.mixNodes {
 		presence := db.mixNodes[key]
-		if presence.LastSeen <= window(5) {
+		if presence.LastSeen <= timeWindow(10) {
 			delete(db.mixNodes, key)
 		}
 	}
 }
 
 // defines staleness
-func window(seconds int64) int64 {
+func timeWindow(seconds int64) int64 {
 	d := time.Duration(-1 * seconds)
 	return timemock.Now().Add(time.Duration(d * time.Second)).Unix()
 }
