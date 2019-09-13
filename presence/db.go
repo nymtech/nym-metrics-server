8f7e36e5318c59f6b79dc24b330e84681ab0e573
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-09-13 14:48:33 +0100

    presence: minor cleanup to sliding timeWindow on staleness

--- presence/db.go
+++ presence/db.go
@@ -37,17 +37,19 @@ func (db *db) get(key string) models.MixNodePresence {
 	return db.mixNodes[key]
 }
 
-// kill any stale presence info
+// kill any stale presence info (older than 5 seconds)
+// TODO: kill magic number 5 by pulling this out into a config
 func (db *db) killOldsters() {
 	for key := range db.mixNodes {
 		presence := db.mixNodes[key]
-		if presence.LastSeen <= cutoff() {
+		if presence.LastSeen <= window(5) {
 			delete(db.mixNodes, key)
 		}
 	}
 }
 
 // defines staleness
-func cutoff() int64 {
-	return timemock.Now().Add(time.Duration(-5 * time.Second)).Unix()
+func window(seconds int64) int64 {
+	d := time.Duration(-1 * seconds)
+	return timemock.Now().Add(time.Duration(d * time.Second)).Unix()
 }
