795e7ffb7ec3c4c6fad41fa8a3e6069a0c7c4c68
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-09-17 14:04:58 +0100

    metrics: fixing fixtures in service test

--- metrics/service_test.go
+++ metrics/service_test.go
@@ -16,13 +16,15 @@ var _ = Describe("metrics.Service", func() {
 
 	// set up fixtures
 	metric1 = models.MixMetric{
-		PubKey: "key1",
-		Sent:   map[string]uint{"mixnode3": 99, "mixnode4": 101},
+		PubKey:   "key1",
+		Received: 99,
+		Sent:     map[string]uint{"mixnode3": 99, "mixnode4": 101},
 	}
 
 	metric2 = models.MixMetric{
-		PubKey: "key2",
-		Sent:   map[string]uint{"mixnode3": 102, "mixnode4": 103},
+		PubKey:   "key2",
+		Received: 99,
+		Sent:     map[string]uint{"mixnode3": 102, "mixnode4": 103},
 	}
 
 	Describe("Adding a mixmetric", func() {
