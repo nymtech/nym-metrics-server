1b04ee05b5fdc54be9bde81ca8c565c807f4d240
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-09-18 13:25:09 +0100

    metrics: periodically clearing the database so we don't run out of memory

--- metrics/db.go
+++ metrics/db.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"sync"
+	"time"
 
 	"github.com/nymtech/directory-server/models"
 )
@@ -9,28 +10,54 @@ import (
 type db struct {
 	sync.Mutex
 	mixMetrics []models.MixMetric
+	ticker     *time.Ticker
 }
 
-// Db holds presence information
+// Db holds metrics information
 type Db interface {
 	Add(models.MixMetric)
 	List() []models.MixMetric
 }
 
 func newMetricsDb() *db {
-	return &db{
+	ticker := time.NewTicker(10 * time.Second)
+
+	d := db{
 		mixMetrics: []models.MixMetric{},
 	}
+	d.ticker = ticker
+	go dbCleaner(ticker, &d)
+
+	return &d
 }
 
+// Add adds a models.MixMetric to the database
 func (db *db) Add(metric models.MixMetric) {
 	db.Lock()
 	defer db.Unlock()
 	db.mixMetrics = append(db.mixMetrics, metric)
 }
 
+// List returns all models.MixMetric in the database
 func (db *db) List() []models.MixMetric {
 	db.Lock()
 	defer db.Unlock()
 	return db.mixMetrics
+}
+
+// dbCleaner periodically clears the database
+func dbCleaner(ticker *time.Ticker, database *db) {
+	for {
+		select {
+		case <-ticker.C:
+			database.clear()
+		}
+	}
+}
+
+// clear kills any stale presence info
+func (db *db) clear() {
+	db.Lock()
+	defer db.Unlock()
+	db.mixMetrics = db.mixMetrics[:0]
 }
